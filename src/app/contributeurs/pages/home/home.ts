e13f34b6d3f5caa9deb72f3f2959e29eda8ca9de
Adama345 <assimtrap@gmail.com>
2025-08-04 15:11:10 +0000

    nouvelle ajout card

--- src/app/contributeurs/pages/home/home.ts
+++ src/app/contributeurs/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Banniere } from '../../components/ui/banniere/banniere';
+import { CardProjectActif } from '../../components/ui/card-project-actif/card-project-actif';
+import { ProjectsRecommander } from '../../components/ui/projects-recommander/projects-recommander';
 
 @Component({
   selector: 'app-home',
-  imports: [Banniere],
+  imports: [Banniere,CardProjectActif,ProjectsRecommander ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
