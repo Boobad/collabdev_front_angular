df1d71ba83a3d11e94c340fdaa6ed5c6798cf81b
Bakary Diallo <bakarydiallo312@gmail.com>
2025-08-04 15:55:29 +0000

    creation formulaire pour la recuperation du password et l'ajout du logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navebar } from './contributeurs/components/shared/navebar/navebar';
 import { Sidebar } from './contributeurs/components/shared/sidebar/sidebar';
+import { ForgetPassword } from "./contributeurs/pages/forget-password/forget-password";
 
 @Component({
   selector: 'app-root',
-  imports: [Sidebar,Navebar, RouterOutlet],
+  imports: [ ForgetPassword],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
