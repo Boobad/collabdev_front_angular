1166d3e9bbbf239fb06799daa50590aa25d0a7aa
Bakary Diallo <bakarydiallo312@gmail.com>
2025-08-05 10:48:06 +0000

    ajout interaction entre la page login et forget-password

--- src/app/contributeurs/pages/forget-password/forget-password.ts
+++ src/app/contributeurs/pages/forget-password/forget-password.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
-  imports: [],
   templateUrl: './forget-password.html',
   styleUrl: './forget-password.css'
 })
 export class ForgetPassword {
+ constructor(private router: Router) {} 
 
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
