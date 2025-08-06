c42941093b31f95755a08e06ca5c77deb244b2ef
Djibril Maïga <maigadjibril444@gmail.com>
2025-08-06 12:22:36 +0000

    Correction du css du bouton envoyer du composant 'Discussion'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { WorkspaceProject } from './contributeurs/pages/workspace-project/workspace-project';
 
 export const routes: Routes = [
 {
-
+  path: 'WorkspaceProject', component: WorkspaceProject
 }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { TaskDetails } from "./contributeurs/pages/task-details/task-details";
+import { WorkspaceProject } from "./contributeurs/pages/workspace-project/workspace-project";
 
 @Component({
   selector: 'app-root',
-  imports: [TaskDetails],
+  imports: [TaskDetails, WorkspaceProject],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
