2ec60405a70e2d642e689d1aead4081cdced2bd1
Bakary Diallo <bakarydiallo312@gmail.com>
2025-08-06 14:48:00 +0000

    page modification projet interaction

--- src/app/contributeurs/pages/upadate-profil/upadate-profil.ts
+++ src/app/contributeurs/pages/upadate-profil/upadate-profil.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upadate-profil',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class UpadateProfil {
 
+constructor(private router: Router) {} 
+
+  redirectToLogin() {
+    this.router.navigate(['/']);
+  }
 }
