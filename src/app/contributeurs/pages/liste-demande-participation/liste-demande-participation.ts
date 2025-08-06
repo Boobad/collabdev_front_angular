1a5923ee8576484572b51799b9acdb9c35a82fc8
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2025-08-06 08:47:50 +0000

    ajou de la liste des demande de participation

---
+++ src/app/contributeurs/pages/liste-demande-participation/liste-demande-participation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-demande-participation',
+  imports: [],
+  templateUrl: './liste-demande-participation.html',
+  styleUrl: './liste-demande-participation.css'
+})
+export class ListeDemandeParticipation {
+
+}
