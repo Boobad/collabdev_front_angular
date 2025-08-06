4fb7edc32c5e46e401b26a02ca9517ab83b373a9
Bakary Diallo <bakarydiallo312@gmail.com>
2025-08-06 12:32:20 +0000

    ajout de la page  modification profil et l'interaction avec la page profil

--- src/app/contributeurs/pages/profil-user/profil-user.ts
+++ src/app/contributeurs/pages/profil-user/profil-user.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../components/shared/sidebar/sidebar";
 import { Navebar } from "../../components/shared/navebar/navebar";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profil-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profil-user.html',
   styleUrl: './profil-user.css'
 })
---
+++ src/app/contributeurs/pages/upadate-profil/upadate-profil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upadate-profil',
+  imports: [],
+  templateUrl: './upadate-profil.html',
+  styleUrl: './upadate-profil.css'
+})
+export class UpadateProfil {
+
+}
