3cb9acecd921c10c120017047e17825e5cd88ecd
Djibril Maïga <maigadjibril444@gmail.com>
2025-08-06 14:51:56 +0000

    Correction dans le css de la page 'Workspace'

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { TaskDetails } from "./contributeurs/pages/task-details/task-details";
 import { WorkspaceProject } from "./contributeurs/pages/workspace-project/workspace-project";
 
 @Component({
   selector: 'app-root',
-  imports: [TaskDetails, WorkspaceProject],
+  imports: [WorkspaceProject],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
