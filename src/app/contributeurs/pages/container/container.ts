932d4c7587ba47ecec94183057657063c783df22
Djibril Maïga <maigadjibril444@gmail.com>
2025-08-06 16:40:22 +0000

    Merge de la branche main et participation-form

--- src/app/contributeurs/pages/container/container.ts
+++ src/app/contributeurs/pages/container/container.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { Sidebar } from "../../components/shared/sidebar/sidebar";
 import { Navebar } from "../../components/shared/navebar/navebar";
 import { RouterOutlet } from '@angular/router';
-import { Home } from '../home/home';
 
 
 @Component({
   selector: 'app-container',
-  imports: [Sidebar, Navebar, RouterOutlet, Home],
+  imports: [Sidebar, Navebar, RouterOutlet],
   templateUrl: './container.html',
   styleUrl: './container.css'
 })
--- src/app/contributeurs/pages/home/home.ts
+++ src/app/contributeurs/pages/home/home.ts
@@ -4,12 +4,11 @@ import { Banniere } from '../../components/ui/banniere/banniere';
 import { CardProjectActif } from '../../components/ui/card-project-actif/card-project-actif';
 import { ProjectsRecommander } from '../../components/ui/projects-recommander/projects-recommander';
 import { CardBadges } from '../../components/ui/card-badges/card-badges';
-import { Navebar } from "../../components/shared/navebar/navebar";
-import { Sidebar } from "../../components/shared/sidebar/sidebar";
+
 
 @Component({
   selector: 'app-home',
-  imports: [Banniere, CardProjectActif, ProjectsRecommander, CardBadges, Navebar, Sidebar, RouterLink],
+  imports: [Banniere, CardProjectActif, ProjectsRecommander, CardBadges, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
